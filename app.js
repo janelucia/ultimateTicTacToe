8ac68991f527631f69766c4e101a899ea6228d11
Jane <janeluciaschoenfeld@gmail.com>
2023-05-10 09:53:50 +0200

    some changes

---
+++ app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const currentPath = window.location.pathname;
+  //   if (currentPath.endsWith('/') || currentPath.endsWith('/index.html')) {
+  //     renderOverviewPage();
+  //   }
+  if (currentPath.endsWith('/game.html')) {
+    boardRender();
+  }
+});
