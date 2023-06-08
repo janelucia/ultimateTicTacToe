ca061db1ad57c0b187dd68698f3da377e0c6286f
Jane <janeluciaschoenfeld@gmail.com>
2023-06-08 13:47:18 +0200

    sort array to showcase the heroes which won

--- client/js/heldentafel.js
+++ client/js/heldentafel.js
@@ -1,6 +1,58 @@
 const spielerListeHolen = async () => {
   const json = await alleSpielerHolen();
-  const data = await json.json();
+  const spieler = await json.json();
+  console.log(spieler);
 
-  console.log(data);
+  const spielerMitGewonnenSpielen = spieler
+    .filter((spieler) =>
+      spieler.spiele.some((spiel) => spiel.gewinner === true)
+    )
+    .map((spieler) => ({
+      ...spieler,
+      spiele: spieler.spiele
+        .filter((spiel) => spiel.gewinner === true)
+        .sort((spielA, spielB) => spielA.zuegeGewinner - spielB.zuegeGewinner),
+    }));
+
+  console.log('1', spielerMitGewonnenSpielen);
+
+  const sortieren = spielerMitGewonnenSpielen.sort((spielerA, spielerB) => {
+    const zuegeA = spielerA.spiele.reduce(
+      (min, spiel) => Math.min(min, spiel.zuegeGewinner),
+      Infinity
+    );
+    const zuegeB = spielerB.spiele.reduce(
+      (min, spiel) => Math.min(min, spiel.zuegeGewinner),
+      Infinity
+    );
+    return zuegeA - zuegeB;
+  });
+
+  console.log('2', sortieren);
+  renderHeldentafel(spielerMitGewonnenSpielen);
 };
+
+function renderHeldentafel(sortiertesArr) {
+  while (table.rows.length > 1) {
+    table.deleteRow(1);
+  }
+
+  sortiertesArr.forEach((spieler) => {
+    const { name, spiele } = spieler;
+    const minZuegeGewinner = Math.min(
+      ...spiele.map((spiel) => spiel.zuegeGewinner)
+    );
+    const spielmodus = spiele.find(
+      (spiel) => spiel.zuegeGewinner === minZuegeGewinner
+    ).spielmodus;
+
+    const reihe = table.insertRow();
+    const nameZelle = reihe.insertCell();
+    const zuegeZelle = reihe.insertCell();
+    const spielmodusZelle = reihe.insertCell();
+
+    nameZelle.textContent = name;
+    zuegeZelle.textContent = minZuegeGewinner.toString();
+    spielmodusZelle.textContent = spielmodus.toLowerCase();
+  });
+}
